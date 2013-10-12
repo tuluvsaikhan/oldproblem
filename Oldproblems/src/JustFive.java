0155c8209e46d187f3a83435abc2bb3d03b2ca9c
Battulga Tuluvsaikhan <donoi2000@gmail.com>
2013-10-12 10:41:59 +0800

    Created 5th problem

---
+++ Oldproblems/src/JustFive.java
@@ -0,0 +1,42 @@
+
+public class JustFive {
+	private static int i=0;
+	private JustFive(){}
+	private static JustFive getInstance() throws Exception{
+		if(i<5) {
+			i++;
+			System.out.println("Instance created "+i);
+			return new JustFive();
+		} else
+			throw new Exception("Can't create more than 5 instances of this class");
+	}
+	public static void main(String[] args) throws Exception {
+		 getInstance();
+		 getInstance();
+		 getInstance();
+		 getInstance();
+		 getInstance();
+	}
+}
+//	class Sample
+//	{
+//	   private static int i = 0;
+//
+//	   private Sample()
+//	   {
+//	   } 
+//
+//	  public static Sample CreateInstance()
+//	   {
+//	      if(i <5)
+//	      {
+//	          i++;
+//	          return new Sample();
+//	      } 
+//
+//	      else
+//	        throw new Exception("Can not create more then 5 instance of this class");  
+//	   }
+//
+//	}
+
--- Oldproblems/src/Question1.java
+++ Oldproblems/src/Question1.java
@@ -1,7 +1,4 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashSet;
-
+import java.util.*;
 public class Question1 {
 
 	public static void main(String[] args) {
