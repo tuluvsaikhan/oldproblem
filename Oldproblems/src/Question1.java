c4e0252aab3154eaba76ca09b6101429c0ae9de4
Battulga Tuluvsaikhan <donoi2000@gmail.com>
2013-10-17 15:47:48 +0800

    new methods using .equals() and hashset

--- Oldproblems/src/Question1.java
+++ Oldproblems/src/Question1.java
@@ -2,20 +2,46 @@ import java.util.*;
 public class Question1 {
 
 	public static void main(String[] args) {
-		String[] words = {"good","better", "best", "best", "first" , "last", "last", "last", "good"};  
-		List<String> list = new ArrayList<String>();  
-		for (String s : words) {  
-		    list.add(s);  
-		}  
-		removeDuplicates(list);
-	}
-    static void removeDuplicates(List<String> array){
-    	HashSet<String> hs = new HashSet<>();
-    	hs.addAll(array);
-    	array.clear();
-    	array.addAll(hs);
-    	for (String x : array){ 
-    		System.out.println(x);
-    	}
-    }
+	    List<String> list = new ArrayList<String>(Arrays.asList(new String[] {
+	        "good", "better", "best", "best", "first", "last", "last", "last",
+	        "good"}));
+	    removeDuplicates(list);
+	    System.out.println("Using equals ");
+	    for (String x : list) {
+	      System.out.println(x);
+	    }
+	  }
+
+	  static void removeDuplicates(List<String> array) {
+	    for (int i = 0; i < array.size(); i++) {
+	      String next = array.get(i);
+
+	      // check if this has already appeared before
+	      for (int j = 0; j < i; j++) {
+	        // if it has, stop the search and remove it
+	        if (next.equals(array.get(j))) {
+	          array.remove(i);
+	          // decrement i since we just removed the i'th element
+	          i--;
+	          // stop the search
+	          break;
+	        }
+	      }
+	    }
+	  }
+	  static void removeDuplicates2(List<String> array) {
+		    Set<String> set = new HashSet<String>();
+
+		    for (int i = 0; i < array.size(); i++) {
+		      String next = array.get(i);
+
+		      // check if this has already appeared before
+		      if (!set.add(next)) {
+		        // if it has then remove it
+		        array.remove(i);
+		        // decrement i since we just removed the i'th element
+		        i--;
+		      }
+		    }
+	  	}
 }
